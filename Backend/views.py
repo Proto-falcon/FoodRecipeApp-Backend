557b3aab5f6b9364686085d350df370702f9f31d
Tobi <tobiradeniji@gmail.com>
2022-11-08 21:19:16 +0000

    removed check for first "_"

--- Backend/views.py
+++ Backend/views.py
@@ -4,13 +4,8 @@ from Backend import FetchFood
 
 # Create your views here.
 def index(request: HttpRequest):
-    query_string = request.META['QUERY_STRING'][12:]
     ingredients = []
-    
-    if query_string[0] == "_":
-        ingredients = query_string[1:].split("_")
-    else:
-        ingredients = request.META['QUERY_STRING'][12:].split("%20")
+    ingredients = request.META['QUERY_STRING'][12:].split("%20")
 
     if len(ingredients) > 0:
         context = FetchFood.fetchfood(ingredients)
